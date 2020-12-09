d2ac00daa5bc8c2d32674536fc8d4c5233f2cafd
stingys <stingys@163.com>
2020-12-09 22:16:09 +0800

    新增登录,注册ts版本

--- src/views/account/Login.vue
+++ src/views/account/Login.vue
@@ -1,19 +1,69 @@
 <template>
-  <div class="login">
-  <a-button type="primary">{{number}}</a-button>
+  <div class="account">
+    <div class="form-warp">
+        <a-form name="custom-validation" ref="ruleForm" v-bind="layout">
+          <a-form-item>
+            <label for="">用户名</label>
+            <a-input autocomplete="off" block/>
+          </a-form-item>
+          
+          <a-form-item>
+            <label for="">密&nbsp;&nbsp;&nbsp;&nbsp;码</label>
+            <a-input type="password" autocomplete="off" block/>
+          </a-form-item>
+        
+          <a-form-item style="margin-top:20px">
+            <a-button type="primary" html-type="submit" block>
+              登录
+            </a-button>
+          </a-form-item>
+
+          <div class="text-center fs-12">
+            <a href="" class="color-white">忘记密码 | </a>
+            <a href="" class="color-white">注册</a>
+          </div>
+        </a-form>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent,ref } from "vue";
+import { defineComponent,reactive,ref, toRefs } from "vue";
 
 export default defineComponent({
   name: "Login",
   setup(props){
-    const number = ref<number>(10)
+    const ruleConfig = reactive<any>({
+      layout:{
+        labelCol: { span: 8 },
+        wrapperCol: { span: 24 },
+      },
+    })
+    const refsRuleConfig = toRefs(ruleConfig)
+    // console.log(refsRuleConfig,'refsRuleConfig')
     return {
-      number
+      // ruleConfig
+      ...refsRuleConfig
     }
   }
 });
 </script>
+
+<style lang="scss" scoped>
+.account {
+  width: 100vw;
+  height: 100vh;
+  background:#282828;
+  .form-warp {
+    width: 285px;
+    margin:auto;
+    padding-top: 300px;
+    color: #fff;
+  }
+  label {
+    display: block;
+    font-size: 14px;
+    color: #fff;
+  }
+}
+</style>
